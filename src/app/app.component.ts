eb85c79d8a1881c760a7a655b89adc158480112e
Toluwanimi I. Akindele <tiakindele11@gmail.com>
2019-01-19 19:32:58 -0700

    sample data on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {formatDate } from '@angular/common';
+import { Validators } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-forms';
+  today = new Date();
+  jstoday = '';
+  constructor() {
+    this.jstoday = formatDate(this.today, 'dd-MM-yyyy hh:mm:ss', 'en-US');
+  }
+
+  userModel = new User(0, 8000, "5678BYNH", 2);
 }
---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor (
+        public quantity: number,
+        public amount: number,
+        public license: string,
+        public customer: number
+    ) {}
+}
